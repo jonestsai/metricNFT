878039ff81a71fd28b4888f9a09431d2a06b7740
jonestsai <jonestsai512@gmail.com>
2022-03-04 18:16:21 -0800

    Move DeGods hourly holder snapshot to Serum RPC

--- server/src/snapshot/cron2.sh
+++ server/src/snapshot/cron2.sh
@@ -2,3 +2,4 @@
 
 /home/server/node_modules/.bin/ts-node /home/server/src/snapshot/stoned-ape-crew.ts >> /home/server/src/snapshot/logs/stoned-ape-crew.log
 /home/server/node_modules/.bin/ts-node /home/server/src/snapshot/taiyo-robotics.ts >> /home/server/src/snapshot/logs/taiyo-robotics.log
+/home/server/node_modules/.bin/ts-node /home/server/src/snapshot/degods.ts >> /home/server/src/snapshot/logs/degods.log
