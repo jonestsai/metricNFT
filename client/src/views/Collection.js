fd818764be3bddb48d3c452699d5f2f18f90aa39
jonestsai <jonestsai512@gmail.com>
2022-03-15 23:22:49 -0700

    Edit spacing

--- client/src/views/Collection.js
+++ client/src/views/Collection.js
@@ -102,7 +102,7 @@ export default class Collection extends React.Component {
           <div>
             <div className="row">
               <div className="col-lg-6">
-                <div className="chart rounded shadow-lg mb-5">
+                <div className="chart rounded shadow-lg mb-4">
                   <h5 className="text-start p-3">Number of Tokens Listed</h5>
                   <ResponsiveContainer width="100%" height={300}>
                     <LineChart
@@ -126,7 +126,7 @@ export default class Collection extends React.Component {
                 </div>
               </div>
               <div className="col-lg-6">
-                <div className="chart rounded shadow-lg mb-5">
+                <div className="chart rounded shadow-lg mb-4">
                   <h5 className="text-start p-3">Number of Owners</h5>
                   <ResponsiveContainer width="100%" height={300}>
                     <LineChart
