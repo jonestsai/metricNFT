6714967cb5e41fbb3f76bb76c3d7f47cefff3a6e
jonestsai <jonestsai512@gmail.com>
2022-04-25 18:40:35 -0700

    Take snapshot from database

---
+++ server/src/snapshot/snapshot.ts
@@ -0,0 +1,110 @@
+import {
+  LAMPORTS_PER_SOL,
+} from '@solana/web3.js';
+require('dotenv').config({ path: '/home/server/.env' });
+const { Pool } = require('pg');
+const fetch = require('node-fetch');
+
+const { marketplaceAddresses } = require('./utils/constants');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+async function main() {
+  // Save snapshot
+  await saveSnapshot();
+}
+
+const saveSnapshot = async () => {
+  const startSnapshotTime = new Date();
+  const { magicEden, magicEdenV2, solanart, digitalEyes, alphaArt, FTX, SMB } = marketplaceAddresses;
+  const { rows } = await pool.query(`
+    SELECT collection_symbol, magiceden_api,
+      COUNT(distinct owner_address) AS owners_count,
+      COUNT(*) FILTER (WHERE owner_address = '${magicEden}'
+        OR owner_address = '${magicEdenV2}'
+        OR owner_address = '${solanart}'
+        OR owner_address = '${digitalEyes}'
+        OR owner_address = '${alphaArt}'
+        OR owner_address = '${FTX}'
+        OR owner_address = '${SMB}') AS listed_count,
+      COUNT(*) FILTER (WHERE owner_address = '${magicEden}') AS magiceden,
+      COUNT(*) FILTER (WHERE owner_address = '${magicEdenV2}') AS magiceden_v2,
+      COUNT(*) FILTER (WHERE owner_address = '${solanart}') AS solanart,
+      COUNT(*) FILTER (WHERE owner_address = '${digitalEyes}') AS digitaleyes,
+      COUNT(*) FILTER (WHERE owner_address = '${alphaArt}') AS alphaart,
+      COUNT(*) FILTER (WHERE owner_address = '${FTX}') AS ftx,
+      COUNT(*) FILTER (WHERE owner_address = '${SMB}') AS smb
+    FROM holder
+    JOIN collection ON holder.collection_symbol = collection.symbol
+    GROUP BY collection_symbol, magiceden_api
+  `);
+  console.log(rows);
+
+  for (let row of rows) {
+    const {
+      collection_symbol,
+      magiceden_api,
+      owners_count,
+      listed_count,
+      magiceden,
+      magiceden_v2,
+      solanart,
+      digitaleyes,
+      alphaart,
+      ftx,
+      smb,
+    } = row;
+    console.log(owners_count);
+    console.log(listed_count);
+
+    let marketplace: any = {
+      'magicEden': magiceden,
+      'magicEdenV2': magiceden_v2,
+      'solanart': solanart,
+      'digitalEyes': digitaleyes,
+      'alphaArt': alphaart,
+      'FTX': ftx,
+      'SMB': smb,       
+    };
+    marketplace = JSON.stringify(marketplace);
+    console.log(marketplace);
+
+    let floorPrice: any;
+
+    try {
+      const response = await fetch(magiceden_api);
+      const collection = await response.json();
+      floorPrice = collection.floorPrice / LAMPORTS_PER_SOL;
+      console.log(floorPrice);
+    } catch (err) {
+      console.log(err);
+    }
+
+    const endSnapshotTime = new Date();
+    console.log(endSnapshotTime);
+
+    const query = {
+      text: 'INSERT INTO snapshot(symbol, starttime, endtime, listedcount, ownerscount, listedmarketplace, floorprice) VALUES($1, $2, $3, $4, $5, $6, $7)',
+      values: [collection_symbol, startSnapshotTime, endSnapshotTime, listed_count, owners_count, marketplace, floorPrice],
+    };
+    pool.query(query, (err: any, res: any) => {
+      console.log(err, res)
+    });
+
+    await new Promise(f => setTimeout(f, 500));
+  }
+}
+
+main().then(
+  () => process.exit(),
+  err => {
+    console.error(err);
+    process.exit(-1);
+  },
+);
---
+++ server/src/snapshot/utils/constants.ts
@@ -0,0 +1,13 @@
+const marketplaceAddresses = {
+  magicEden: 'GUfCR9mK6azb9vcpsxgXyj7XRPAKJd4KMHTTVvtncGgp',
+  magicEdenV2: '1BWutmTvYPwDtmw9abTkS4Ssr8no61spGAvW1X6NDix',
+  solanart: '3D49QorJyNaL4rcpiynbuS3pRH4Y7EXEM6v6ZGaqfFGK',
+  digitalEyes: 'F4ghBzHFNgJxV4wEQDchU5i7n4XWWMBSaq7CuswGiVsr',
+  alphaArt: '4pUQS4Jo2dsfWzt3VgHXy3H6RYnEDd11oWPiaM2rdAPw',
+  FTX: '73tF8uN3BwVzUzwETv59WNAafuEBct2zTgYbYXLggQiU',
+  SMB: '7Ppgch9d4XRAygVNJP4bDkc7V6htYXGfghX4zzG9r4cH',
+};
+
+module.exports = {
+  marketplaceAddresses,
+};
