572f76f7472f2565473d89add1a8a5e6d45a3d5f
jonestsai <jonestsai512@gmail.com>
2022-05-16 19:52:16 -0700

    Add #C3 hashtag for communi3 sales

--- twitter/create-tweet.js
+++ twitter/create-tweet.js
@@ -281,7 +281,7 @@ const newTweet = async (tokenAddress, name, price, id) => {
         // file path
         client.v1.uploadMedia('./img.jpg'),
       ]);
-      await client.v1.tweet(`${name}\n\n💵 SOLD for ${price} SOL\n\n🛒 https://magiceden.io/item-details/${tokenAddress}\n\n🧾 https://solscan.io/tx/${id}\n\n#Communi3 #SolanaNFTs #Solana`, { media_ids: mediaIds });
+      await client.v1.tweet(`${name}\n\n💵 SOLD for ${price} SOL\n\n🛒 https://magiceden.io/item-details/${tokenAddress}\n\n🧾 https://solscan.io/tx/${id}\n\n#Communi3 #C3 #SolanaNFTs #Solana`, { media_ids: mediaIds });
     }
   } catch (error) {
     console.log(error);
