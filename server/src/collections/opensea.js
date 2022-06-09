79989f73b4cb1daf8115d18942778c497271bf85
jonestsai <jonestsai512@gmail.com>
2022-06-09 12:58:04 -0700

    Go to the next collection if a collection has no stats

--- server/src/collections/opensea.js
+++ server/src/collections/opensea.js
@@ -109,7 +109,7 @@ const snapshotCollectionStats = async () => {
       collectionStats = await getCollectionStats(slug);
     }
     if (!collectionStats) {
-      collectionStats = await getCollectionStats(slug);
+      continue;
     }
     const { one_day_volume, one_day_change, one_day_sales, one_day_average_price, seven_day_volume, seven_day_change, seven_day_sales, seven_day_average_price, thirty_day_volume, thirty_day_change, thirty_day_sales, thirty_day_average_price, total_volume, total_sales, total_supply, count, num_owners, average_price, num_reports, market_cap, floor_price } = collectionStats;
     const startSnapshotTime = new Date();
