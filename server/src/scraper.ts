f9ae8668f0c1b9d069d923408d8b8ae6fce957a5
jonestsai <jonestsai512@gmail.com>
2022-07-02 01:03:09 -0700

    Cleanup code

--- server/src/scraper.ts
+++ server/src/scraper.ts
@@ -17,50 +17,56 @@ async function main() {
   // const useragent = require('random-useragent');
   // const randomUseragent = randomUseragent.getRandom(); // gets a random user agent string
 
-  let price: any;
-  let data: any;
-
   try {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const [page] = await browser.pages();
-
-    // Get Magic Eden price
     await page.setUserAgent(userAgent.toString());
-    await page.goto('https://api-mainnet.magiceden.io/rpc/getCollectionEscrowStats/degods?edge_cache=true', { waitUntil: 'networkidle0' });
-    // const data = await page.evaluate(() => document.querySelector('*').outerHTML);
-    data = await page.$eval('pre', (element: any) => element.textContent);
 
-    price = JSON.parse(data)?.results?.floorPrice / LAMPORTS_PER_SOL;
-    console.log(price);
-
-    await page.goto('https://api-mainnet.magiceden.io/rpc/getCollectionHolderStats/degods', { waitUntil: 'networkidle0' });
-    data = await page.$eval('pre', (element: any) => element.textContent);
-    const totalSupply = JSON.parse(data)?.results?.totalSupply;
-    const uniqueHolders = JSON.parse(data)?.results?.uniqueHolders;
-    console.log(totalSupply);
-    console.log(uniqueHolders);
-
-    // Get Opensea slugs
-    await page.goto('https://opensea.io/rankings?chain=ethereum&sortBy=total_volume', { waitUntil: 'networkidle0' });
-    await page.waitForTimeout(30000);
-    data = await page.$eval('*', (element: any) => element.textContent);
-    // console.log(data);
-
-    const substrings = data.split('"slug":"');
-    substrings.shift(); // Remove first item since it's not a slug
-
-    let slugs: string[] = [];
-    for (const substring of substrings) {
-      slugs.push(substring.substr(0, substring.indexOf('"')));
-    }
-
-    const uniqueSlugs = [...new Set(slugs)];
-    console.log(uniqueSlugs);
+    // MagicEden
+    await getMagicedenPrice(page);
+    await getMagicedenHolders(page);
+    await getOpenseaSlugs(page);
 
     await browser.close();
   } catch (err) {
     console.error(err);
   }
+}
+
+const getMagicedenPrice = async (page: any) => {
+  await page.goto('https://api-mainnet.magiceden.io/rpc/getCollectionEscrowStats/degods?edge_cache=true', { waitUntil: 'networkidle0' });
+  // const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+  const data = await page.$eval('pre', (element: any) => element.textContent);
+
+  const price = JSON.parse(data)?.results?.floorPrice / LAMPORTS_PER_SOL;
+  console.log(price);
+}
+
+const getMagicedenHolders = async (page: any) => {
+  await page.goto('https://api-mainnet.magiceden.io/rpc/getCollectionHolderStats/degods', { waitUntil: 'networkidle0' });
+  const data = await page.$eval('pre', (element: any) => element.textContent);
+  const totalSupply = JSON.parse(data)?.results?.totalSupply;
+  const uniqueHolders = JSON.parse(data)?.results?.uniqueHolders;
+  console.log(totalSupply);
+  console.log(uniqueHolders);
+}
+
+const getOpenseaSlugs = async (page: any) => {
+  await page.goto('https://opensea.io/rankings?chain=ethereum&sortBy=total_volume', { waitUntil: 'networkidle0' });
+  await page.waitForTimeout(30000);
+  const data = await page.$eval('*', (element: any) => element.textContent);
+  // console.log(data);
+
+  const substrings = data.split('"slug":"');
+  substrings.shift(); // Remove first item since it's not a slug
+
+  let slugs: string[] = [];
+  for (const substring of substrings) {
+    slugs.push(substring.substr(0, substring.indexOf('"')));
+  }
+
+  const uniqueSlugs = [...new Set(slugs)];
+  console.log(uniqueSlugs);
 }
 
 main().then(
