d48a2f3ac62bd9ce157fbdea35246430032f0877
jonestsai <jonestsai512@gmail.com>
2022-11-01 16:39:11 -0700

    Delete sale records that are no longer used

--- server/src/sales/utils/constants.ts
+++ server/src/sales/utils/constants.ts
@@ -1,40 +1,9 @@
 const royaltyAddresses = {
-  'PUFFgnKKhQ23vp8uSPwdzrUhEr7WpLmjM85NB1FQgpb': 'SAC', // SAC
-  'GdtkQajEADGbfSUEBS5zctYrhemXYQkqnrMiGY7n7vAw': 'PRTL', // PRTL
   '9FYsKrNuEweb55Wa2jaj8wTKYDBvuCG3huhakEj96iN9': 'DAPE', // DAA
-  'DRGNjvBvnXNiQz9dTppGk1tAsVxtJsvhEmojEfBU3ezf': 'BORYOKU', // Boryoku Dragonz
-  '8hTYASw98ZCZJwuEF9apPhmQTW7TYaoh9AnJfeV2X5tx': 'MNDFLK', // Mindfolk
-  '8Kag8CqNdCX55s4A5W4iraS71h6mv6uTHqsJbexdrrZm': 'AUROR', // Aurory
-  'AxFuniPo7RaDgPH6Gizf4GZmLQFc4M5ipckeeZfkrPNn': 'DGOD', // DeGods
-  '7wzoWjLRJRVKMR7PhGffpXBpBsCqLRks2zb3Cnap8PZ3': 'Nyan', // Nyan Hero
-  'D6wZ5U9onMC578mrKMp5PZtfyc5262426qKsYJW7nT3p': 'SSC', // SSC
-  '3BjpoZic969Wh8dvgYApuCKL3v5nzgpLAekvumD44qZJ': 'SMB', // SMB (works after 2021-11-25 11:49:02+00)
-  'CDgbhX61QFADQAeeYKP5BQ7nnzDyMkkR3NEhYF2ETn1k': 'TAIYO', // Taiyo
-  'AvkbtawpmMSy571f71WsWEn41ATHg5iHw27LoYJdk8QA': 'THUG', // Thugbirdz
-  'CzrE3LhijwcmvsXZa8YavqgR9EzW3UGqoSWZKwGpZVqM': 'THUG', // Thugbirdz2
-  '9Da5CoqR8H4YGWEYK6jtcTU69rr6XEkffadN8UFjJkeA': 'CoC', // CoC
-  '7zL7HVn85F5yFT6XM3BsJcQF7PBcNE7R2BT5GyfunpKe': 'OKB', // OKB
-  'BSLiqdvTiCLSkpRasjBBJQqNHRLuvp4vP2qyzKALjs9W': 'BSL', // BSL
-  'Em4DcHQwUxhHfEWhz8aZABXU6nUADTGFBPKHoBhKZr9h': 'SOLGods', // SOLGods
-  '6WQPJrQBHwXGNiAffuYt5v96FzS3c4W6Hfi7dvoNkNX4': 'CWM', // Catalina Whale Mixer
-  '3pMvTLUA9NzZQd4gi725p89mvND1wRNQM3C8XEv1hTdA': 'FFF', // Famous Fox Federation
-  '74q6RGYyDsjNAjynqyx6P5SGh9HimS1p9TSLb1Z8RTM8': 'BOHEMIA', // Bohemia
-  'AfVySHXxTrf5vvLMRCNdfeWmE9h6QFsxzxJ2fgJj7VrQ': 'ASTRALS', // Astrals
-  'TSk7aNjyggudgUXzDsdTUmB7uMoNRnpRpXpDPuGfRtE': 'TSHS', // Tombstoned High Society
+  '7zL7HVn85F5yFT6XM3BsJcQF7PBcNE7R2BT5GyfunpKe': 'OKB', // OKB (okay_bears)
   'trshC9cTgL3BPXoAbp5w9UfnUMWEJx5G61vUijXPMLH': 'DTP', // Degen Trash Pandas
-  'RTp26f9wY2fXxeWRE7FkS9iVrsuxgdUJfDYH8GgoBH9': 'GRIM', // Grim Syndicate
-  'FDL3AT32bAAHEC9wujkesnHCvBEQWnmUVuDMJ7T8NJSw': 'QT', // Quantum Traders
-  'pEsKYABNARLiDFYrjbjHDieD5h6gHrvYf9Vru62NX9k': 'NOOT', // Pesky Penguins
-  '9UwSF2gPqzssisA94NoQgzEyDmW2KPGAGBi4o34FK6pH': 'DP', // Defi Pirates
   'AMomyD8PjPLDgxfjh12SGyFXVS6UrNKjdXrwC444dnbe': 'DEGG', // Degenerate Ape Kindergarten
-  'JCTZQ7fzLkS9j3z4bmwmsJuqzpDyqxLB2NwdGdajk2Wm': 'JCATS', // Jungle Cats
   '3497cwzfTv54tUqi5X1GHoDPARKiedapFPNZN1HZbd3T': 'C3', // Communi3
-  '5c186p4RW1nuLZrYHgxv4m1vBPtfkSP7hQTZbqPLdaXH': 'SH', // Space Heads
-  'Br9TQj8DJVFgwocvwc2h47Kms9zjwpaDukuP4ZRmp29s': 'MIDH', // MOUS IN DA HOUS
-  'EgFY9PKziUJeKBwsQAH9zuvKyZLvx37gqtxmj5QtN1CX': 'TSF', // The Stoned Frogs
-  'rVTWZ3PyCf3PAAJzVcXgs3v6xkFFfCK2M5tQSZ2MUBh': 'ATD', // OG Atadians
-  'HZ95a2ADYQbKERW8Ts939MQtyy44UFB84oPQMb6rUaqC': 'OMNI', // The Bridged
-  '9bv3v4DFoXSBCyCRepp8gQYUMdjfjZVeR9qyoPmhz6Uv': 'LIZARD', // Reptilian Renegade (Lizards)
   'MF6wYTno1WMneM2bGaSDdFBJm8dehB2sQ1Dq8PBmpbc': 'TAT', // Trippin' Ape Tribe
 };
 
