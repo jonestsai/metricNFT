6afae0697c8e48542312e75f3dc908be74eae127
jonestsai <jonestsai512@gmail.com>
2022-11-19 17:59:39 -0800

    Make secondary menu scrollable on mobile

--- client/src/components/layout/SecondaryMenu.js
+++ client/src/components/layout/SecondaryMenu.js
@@ -6,9 +6,9 @@ export default function SecondaryMenu(props) {
   const location = useLocation();
 
   return (
-    <Nav className="secondary-menu px-4 border-bottom border-secondary" variant="tabs" activeKey={location.pathname}>
+    <Nav className="secondary-menu px-4 border-bottom border-secondary overflow-auto text-nowrap" variant="tabs" activeKey={location.pathname}>
       <Nav.Item>
-        <Nav.Link className="d-flex" href="/watchlist"><FaStar className="me-2" size={20} style={{ height: 25 }} role="button" color="#fc6" />Watchlist</Nav.Link>
+        <Nav.Link href="/watchlist"><FaStar className="me-2" size={20} style={{ height: 20 }} role="button" color="#fc6" />Watchlist</Nav.Link>
       </Nav.Item>
       <Nav.Item>
         <Nav.Link href="/">Collections</Nav.Link>
