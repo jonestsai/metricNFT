258620ff66247593542a38939355affc7f490349
jonestsai <jonestsai512@gmail.com>
2022-12-03 19:34:33 -0800

    Scroll to top of page on route change

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 
 ReactDOM.render(
   <BrowserRouter>
+    <ScrollToTop />
     <App />
   </BrowserRouter>,
   document.getElementById('root')
