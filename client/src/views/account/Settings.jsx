c49bbda517be71943fb54f07f3d1498f8dbf8767
jonestsai <jonestsai512@gmail.com>
2022-12-03 18:15:41 -0800

    Show email field only after loading

--- client/src/views/account/Settings.jsx
+++ client/src/views/account/Settings.jsx
@@ -64,32 +64,36 @@ export default function Settings() {
 
   return (
     <div className="container-fluid py-5 px-sm-0">
-      <div className="row justify-content-md-center">
-        <div className="col-md-6">
-          <FloatingLabel
-            controlId="floatingInput"
-            label="Email address"
-            className="mb-3"
-          >
-            <Form.Control
-              required
-              type="email"
-                value={email}
-              onChange={e => setEmail(e.target.value)}
-            />
-          </FloatingLabel>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-6 offset-md-3 d-flex justify-content-between">
-          <button type="button" className="btn btn-primary" onClick={() => handleEmail('connect')}>Save</button>
-          {isEmailSaved && (
-            <div className="text-success my-1">Saved!</div>
-          )}
-          <button type="button" className="btn btn-outline-danger" onClick={() => handleEmail('disconnect')}>Disconnect Email</button>
+      {(publicKey && !isLoading) && (
+        <div>
+          <div className="row justify-content-md-center">
+            <div className="col-md-6">
+              <FloatingLabel
+                controlId="floatingInput"
+                label="Email address"
+                className="mb-3"
+              >
+                <Form.Control
+                  required
+                  type="email"
+                    value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
+              </FloatingLabel>
+            </div>
           </div>
-      </div>
-      {(publicKey && isLoading) && (
+          <div className="row">
+            <div className="col-md-6 offset-md-3 d-flex justify-content-between">
+              <button type="button" className="btn btn-primary" onClick={() => handleEmail('connect')}>Save</button>
+              {isEmailSaved && (
+                <div className="text-success my-1">Saved!</div>
+              )}
+              <button type="button" className="btn btn-outline-danger" onClick={() => handleEmail('disconnect')}>Disconnect Email</button>
+              </div>
+          </div>
+        </div>
+      )}
+      {isLoading && (
         <div className="my-5 text-center">
           <div className="spinner-border text-light" role="status" />
         </div>
